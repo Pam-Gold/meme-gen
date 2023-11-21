379460d607e78e0e7f618eb4abaafd4f07f38be6
PamGold <badtrailz@gmail.com>
2023-11-21 15:59:41 +0100

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
 
-function App() {
+export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMeme, setAllMeme] = useState([]);
+
+  useEffect(() => {
+    console.log("Effect ran");
+    const apiUrl = `https://api.imgflip.com/get_memes`;
+
+    fetch(apiUrl)
+      .then((rs) => rs.json())
+      .then((data) => setAllMeme(data.data.memes));
+  }, []);
+
+  function getMemeImage(data) {
+    const randomNumber = Math.floor(Math.random() * allMeme.length);
+    const url = allMeme[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+
+    // getMemeImage(data)
+  }
+
+  function handleChanges(e) {
+    let { name, value } = e.target;
+
+    setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          onChange={handleChanges}
+          name="topText"
+          value={meme.topText}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          onChange={handleChanges}
+          name="bottomText"
+          value={meme.bottomText}
+        />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
   );
 }
-
-export default App;
